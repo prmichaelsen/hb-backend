e5be1b9e77c47a2adfe81c523240a4f388367c20
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2018-11-21 00:17:20 -0700

    cleanup: remove unused packages

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,6 +1,5 @@
 import get from './get';
 import { delay } from './utils';
-import * as fetch from 'node-fetch';
 import {
 	rh,
 	} from './helpers';
@@ -8,7 +7,7 @@ import {
 	Options,
 	} from 'robinhood';
 import {
-	isNullOrUndefined, promisify,
+	isNullOrUndefined,
 	} from 'util';
 import _ = require('lodash');
 
