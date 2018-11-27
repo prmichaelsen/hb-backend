d8ed09c77b12ef3c20bd49ddc15822d889ee99b1
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2018-11-26 17:59:42 -0700

    cleanup: move node-cron jobs into cron

---
+++ src/cron.ts
@@ -0,0 +1,98 @@
+import { firebase } from './firebase';
+import {
+	dateMarketCloses,
+	dateMarketOpens,
+	getMarketHours,
+	MarketHours,
+	tradier
+	} from './tradier';
+import { sanitize } from '@prmichaelsen/ts-utils';
+import { CronJob } from 'cron';
+import moment = require("moment");
+
+const db = firebase.database();
+
+/**
+ * runs every month at midnight NYSE time
+ * to update the database with market hours
+ * for this month and next
+ */
+const onUpdateMonthlyMarketHours = async () => {
+	const thisMonth = moment();
+	const nextMonth = moment().add({ month: 1 });
+	const marketHours: MarketHours[] = [
+		...getMarketHours(await tradier.calendar({
+			month: String(thisMonth.month() + 1),
+			year: String(thisMonth.year()),
+		})),
+		...getMarketHours(await tradier.calendar({
+			month: String(nextMonth.month() + 1),
+			year: String(nextMonth.year()),
+		})),
+	];
+	await db.ref('market/meta/marketHours').set(marketHours);
+}
+new CronJob({
+	// at 00:00:00 on day-of-month 1
+	cronTime: '0 0 0 1 * *',
+	timeZone: 'America/New_York',
+	start: true,
+	onTick: onUpdateMonthlyMarketHours,
+});
+
+/**
+ * runs every day at midnight NYSE time
+ * to update the database with market hours
+ * for today
+ */
+const onUpdateTodaysMarketHours = async () => {
+	const marketHours: MarketHours[] = (
+		await db.ref('market/meta/marketHours').once('value')
+	).val() || [];
+	await db.ref('market/meta').update(sanitize({
+		dateMarketCloses: dateMarketCloses(marketHours),
+		dateMarketOpens: dateMarketOpens(marketHours),
+	}));
+};
+new CronJob({
+	// at 00:00:00
+	cronTime: '0 0 0 * * *',
+	timeZone: 'America/New_York',
+	start: true,
+	onTick: onUpdateTodaysMarketHours,
+});
+
+/**
+ * runs every second in order to update
+ * the database with whether or not
+ * the market is currently open.
+ */
+const onUpdateMarketIsOpen = async () => {
+	const now = moment();
+	const dateMarketCloses = moment((
+		await db.ref('market/meta/dateMarketCloses').once('value')
+	).val());
+	const dateMarketOpens = moment((
+		await db.ref('market/meta/dateMarketOpens').once('value')
+	).val());
+	if (!dateMarketCloses.isValid() || !dateMarketOpens.isValid()) {
+		return;
+	}
+	const isOpen = now.isAfter(dateMarketOpens) && now.isBefore(dateMarketCloses);
+	await db.ref('market/meta/isOpen').set(isOpen);
+};
+new CronJob({
+	// every second
+	cronTime: '0/1 0 0 * * *',
+	timeZone: 'America/New_York',
+	start: true,
+	onTick: onUpdateMarketIsOpen,
+});
+
+// fire off jobs, in case they have
+// never been run before
+(async function seedJobs() {
+	await onUpdateMonthlyMarketHours();
+	await onUpdateTodaysMarketHours();
+	await onUpdateMarketIsOpen();
+})();
