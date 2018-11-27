2f37f99ef84124aeed8bda518b42a3c8b5d6eb53
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2018-11-26 17:54:20 -0700

    cleanup: move and improve robinhood types

--- src/robinhood.ts
+++ src/robinhood.ts
@@ -1,5 +1,6 @@
 import * as rhapi from 'robinhood';
 
+/** return a valid robinhood client, assuming a valid token is provided */
 export const robinhood = (token: string, cb: (api: rhapi.RobinhoodWebApi) => void) => {
 	const _rh = (token: string, rhInitCb: () => void) => {
 		const credentials = { token };
@@ -9,4 +10,35 @@ export const robinhood = (token: string, cb: (api: rhapi.RobinhoodWebApi) => voi
 		return rhapi(credentials, rhInitCb);
 	}
 	const r = _rh(token, () => cb(r));
+}
+
+/** types for Robinhood api responses */
+export namespace Robinhood {
+	/**
+	 * robinhood sends 200s with the following body
+	 * when there is a bad request.
+	 */
+	export interface Problem {
+		details: string;
+	}
+
+	/** a place or buy order */
+	export namespace Order {
+		/** the state of the order */
+		export type state = 'queued' | 'unconfirmed' | 'confirmed'
+			| 'partially_filled' | 'filled'
+			| 'reject' | 'canceled' | 'failed';
+
+		/**
+		 * the expected result on a
+		 * valid 200
+		 */
+		export interface Success {
+			/** the state of the order */
+			state: state;
+			[k: string]: any;
+		}
+		/** defines the expected body of the response */
+		export type Body = Problem | Success;
+	}
 }
