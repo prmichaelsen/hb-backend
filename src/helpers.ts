f4473e8eb5801cd31af01632091388cd24d36f70
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2018-11-26 18:00:04 -0700

    cleanup: docs and cleanup

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,6 +4,10 @@ import { Job } from '@prmichaelsen/hb-common';
 import { RobinhoodWebApi } from 'robinhood';
 import { isNullOrUndefined } from 'util';
 
+/**
+ * retrieve the user's robinhood token for this job and
+ * authenticate a robinhood api client
+*/
 export const rh = (job: Job.Job): Promise<RobinhoodWebApi> => new Promise((resolve, reject) => {
 	firebase.database().ref(['robinhood-tokens', job.userId].join('/'))
 		.once('value', snapshot => {
